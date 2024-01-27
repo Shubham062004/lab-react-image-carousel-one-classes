4a63d658e0c09bfd2c3b5ec01a592533c2d0b4c6
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-01-27 11:26:18 +0530

    update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,6 +7,67 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
+     constructor(props) {
+       super(props)
+     
+       this.state = {
+        currentSlide:0
+       }
+
+     }
+
+     nextSlide = () => {
+      let newSlide =
+        this.state.currentSlide === images.length - 1
+          ? 0
+          : this.state.currentSlide + 1;
+      this.setState({ currentSlide: newSlide });
+    };
+  
+    prevSlide = () => {
+      let newSlide =
+        this.state.currentSlide === 0
+          ? images.length - 1
+          : this.state.currentSlide - 1;
+      this.setState({ currentSlide: newSlide });
+    };
+    
+  render(){
+    return(
+        <div className="slider">
+          <nav id="back-nav">
+           <ArrowBackIosIcon className="back-arrow" onClick={this.prevSlide}/>
+           </nav>
+           <nav id="next-nav">
+           <ArrowForwardIosIcon className="next-arrow" onClick = {this.nextSlide}/>
+           </nav>
+            
+           {images.map((slide, imgState) => {
+              return (
+                <>
+                
+                <h1 id="title" className={imgState === this.state.currentSlide
+                    ? "slide.active"
+                    : "slide"}>{slide.title}</h1>
+                    
+
+                    <h3 id="subtitle" className={imgState === this.state.currentSlide
+                    ? "slide.active"
+                    : "slide"}>{slide.subtitle}</h3>
+                <img
+                  src={slide.img}
+                  alt="This is a carousel slide"
+                  className={imgState === this.state.currentSlide
+                    ? "slide-active"
+                    : "slide"} /></>
+              );
+            })}
+            
+            
+
+        </div>
+    )
+  }
   
 }
 
